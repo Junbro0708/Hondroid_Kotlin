a95f7b75e3bf823a8517c2389d4742f6273aaa0e
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-08-24 14:24:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hondroid_pr/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.hondroid_pr
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_a.setOnClickListener {
+            // val 은 final과 동일하게 값이 변경되지 않고, var은 변수
+
+            val intent = Intent(this, SubActivity::class.java) // 다음 화면으로 이동하기 위한 객체 생성
+            intent.putExtra("msg", tv_sendMsg.text.toString()) // 키값과 보내는 값을 넣어주어야함
+            startActivity(intent)
+            finish() // 화면을 전환 후 메인 액티비티가 필요가 없어지면, 자신의 액티비티를 제거
+        }
+    }
+}
